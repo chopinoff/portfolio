8f724110ce23fa5d8c472aa77451e0d10144a93b
chopinoff <chopinoff@gmail.com>
2023-05-27 03:29:32 +0900

    feat : ESLint 설정 및 plugin 설치

--- chopinoff-portfolio/.eslintrc.cjs
+++ chopinoff-portfolio/.eslintrc.cjs
@@ -1,14 +1,40 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parser: "@typescript-eslint/parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  plugins: [
+    "react-refresh",
+    "eslint-plugin-simple-import-sort",
+    "@typescript-eslint",
+    "unused-imports",
+  ],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    "react-refresh/only-export-components": "warn",
   },
-}
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "react/react-in-jsx-scope": "off",
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        "@typescript-eslint/no-shadow": ["error"],
+        "no-unused-vars": "off",
+        "unused-imports/no-unused-imports-ts": ["error"],
+        "no-shadow": "off",
+        "no-undef": "off",
+        "prettier/prettier": [
+          "error",
+          {
+            endOfLine: "auto",
+          },
+        ],
+      },
+    },
+  ],
+};
