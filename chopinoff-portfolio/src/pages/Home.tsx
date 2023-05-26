b34c8d3b5de31b87a0d6a6fbd0b761be27b5b33c
chopinoff <chopinoff@gmail.com>
2023-05-27 03:04:51 +0900

    feat : Home 페이지 작성 및 mq (facepaint) 테스트 완료

---
+++ chopinoff-portfolio/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import { homeWrap } from "./styles/home.style";
+
+const Home = () => {
+  return (
+    <div>
+      <div css={homeWrap}></div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ chopinoff-portfolio/src/pages/styles/home.style.ts
@@ -0,0 +1,4 @@
+import { css } from "@emotion/react";
+import mq from "@/utils/mq";
+
+export const homeWrap = css``;
